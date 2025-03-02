522aea70999e6b192bb534eb0b537ad12bcb39c2
Arijit Sarkar <arijitsrkr13@gmail.com>
2025-03-02 14:23:02 +0530

    Changes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -3,33 +3,45 @@ const Job = require("../models/jobModel");
 const logger = require("../config/logger");
 
 const getAllJobs = async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-
-    const jobs = await Job.find({})
-      .sort({ createdAt: -1 })
-      .skip(skip)
-      .limit(limit)
-      .exec();
-
-    const totalJobs = await Job.countDocuments();
-
-    return res.status(200).json({
-      success: true,
-      data: jobs,
-      pagination: {
-        totalJobs,
-        currentPage: page,
-        totalPages: Math.ceil(totalJobs / limit),
-        hasNextPage: skip + limit < totalJobs,
-      },
-    });
-  } catch (error) {
-    logger.error("Error fetching jobs", { error });
-    res.status(500).json({ success: false, message: "Failed to fetch jobs" });
-  }
-};
-
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
+  
+      const jobTitle = req.query.jobtitle || '';
+      const companyName = req.query.companyname || '';
+  
+      const searchQuery = {};
+      
+      if (jobTitle) {
+        searchQuery.jobTitle = { $regex: jobTitle, $options: 'i' };
+      }
+      
+      if (companyName) {
+        searchQuery.companyName = { $regex: companyName, $options: 'i' };
+      }
+  
+      const jobs = await Job.find(searchQuery)
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .exec();
+  
+      const totalJobs = await Job.countDocuments(searchQuery);
+  
+      return res.status(200).json({
+        success: true,
+        data: jobs,
+        pagination: {
+          totalJobs,
+          currentPage: page,
+          totalPages: Math.ceil(totalJobs / limit),
+          hasNextPage: skip + limit < totalJobs,
+        },
+      });
+    } catch (error) {
+      logger.error("Error fetching jobs", { error });
+      res.status(500).json({ success: false, message: "Failed to fetch jobs" });
+    }
+  };
 module.exports = { getAllJobs };
